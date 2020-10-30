5d3c2d8b4f084e6d11ead8b9835c99c8b99b0f97
Patryk Krakos <redpat20@gmail.com>
2020-10-30 16:27:34 +0100

    Added waiting for addresses creation

--- setConfig.py
+++ setConfig.py
@@ -6,10 +6,10 @@ address_file = sys.argv[1]
 with open ('./' + address_file) as f:
 	address = f.read().splitlines()[0]
 
-with open('config.toml') as f:
+with open('config_empty.toml') as f:
     data = toml.load(f)
     data['Eth']['Miner']['Etherbase'] = address
 
 
-with open('/l16/config.toml', 'w') as f:
+with open('config.toml', 'w') as f:
     toml.dump(data, f)
--- setup-node-geth.sh
+++ setup-node-geth.sh
@@ -3,6 +3,9 @@
 #Will be used to identify address
 export NODE_NAME=$(hostname)
 
+#Set number of expected validators
+export AUTHORITHIES=5
+
 cd /root
 
 sudo apt-get update
@@ -26,17 +29,50 @@ sudo chmod +x /l16/geth
 /l16/geth --datadir /l16/chain_data account new --password /l16/node.pwds | grep -Eo '0x[a-fA-F0-9]{40}' > $NODE_NAME.txt
 
 #Put address into config
-python3 setConfig.py $NODE_NAME.txt
+python3 setConfig.py $NODE_NAME.txt && \
+sudo cp ./config.toml /l16/
 
+#Upload address into common bucket
 gsutil cp ./$NODE_NAME.txt gs://l16-common/addresses
+
+#Create and upload .lock file
 touch create.lock
 gsutil cp ./create.lock gs://l16-common/addresses
 
-if [ `gsutil du gs://l16-common/addresses/*.txt | wc -l` -eq 5 ]
+#Wait until all nodes will deliver their addresses
+while [ `gsutil du gs://l16-common/addresses/*.txt | wc -l` -ne 5  ]; do
+	#do nothing
+	sleep 5
+done
+
+#Is there genesis already?
+if [ `gsutil du gs://l16-common/genesis-geth.json | wc -l` -eq 1 ]
   then
-    echo "All addresses have been created, proceeding to create genesis."
+  	#Yes, let's download it.
+  	echo "Downloading genesis"
+    gsutil cp gs://l16-common/genesis-geth.json .
+    sudo cp ./genesis-geth.json /l16/
+  	
+  else
+    #No, let's create it and upload to bucket.
+  	echo "All addresses have been created, proceeding to create genesis."
     gsutil rm gs://l16-common/addresses/create.lock
     gsutil cp -r gs://l16-common/addresses .
-    python3 createGenesis.py
+    python3 createGenesis.py &&  \
+    gsutil cp genesis-geth.json gs://l16-common/
 
 fi
+
+#Initialize blockchain
+sudo /l16/geth --datadir /l16/chain_data init /l16/genesis-geth.json
+
+#daemonize process
+sudo cp ./geth.service /etc/systemd/system
+sudo chmod +x /etc/systemd/system/geth.service
+
+#START!
+echo "Starting geth..."
+
+sudo systemctl enable geth
+
+#sudo systemctl start geth
