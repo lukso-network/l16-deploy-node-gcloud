6ef3ce0549a256e813a900b0d141196113f1e35a
Patryk Krakos <redpat20@gmail.com>
2020-11-04 18:11:40 +0100

    New geth version

--- setConfig.py
+++ setConfig.py
@@ -6,7 +6,7 @@ address_file = sys.argv[1]
 with open ('./' + address_file) as f:
 	address = f.read().splitlines()[0]
 
-with open('config_empty.toml') as f:
+with open('configEmpty.toml') as f:
     data = toml.load(f)
     data['Eth']['Miner']['Etherbase'] = address
 
