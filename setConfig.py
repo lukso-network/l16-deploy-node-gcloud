193fc6ee65491abfe4e6250195d8f407e017a959
Patryk Krakos <redpat20@gmail.com>
2020-11-04 17:41:14 +0100

    Reverted setConfig.py

--- setConfig.py
+++ setConfig.py
@@ -1,16 +1,7 @@
-import os
 import sys
 import toml
 
 address_file = sys.argv[1]
-ip_addresses =[]
-
-for file in os.scandir('./ip'):
-     filename = os.fsdecode(file)
-     if filename.endswith(".txt"):
-         with open(filename) as f:
-             ip_address = f.read().strip().splitlines()[0]
-             ip_addresses.append(ip_address)
 
 with open ('./' + address_file) as f:
 	address = f.read().splitlines()[0]
@@ -18,7 +9,6 @@ with open ('./' + address_file) as f:
 with open('configEmpty.toml') as f:
     data = toml.load(f)
     data['mining']['engine_signer'] = address
-    data['network']['bootnodes'] = ip_addresses
 
 
 with open('config.toml', 'w') as f:
