06a4adf964d0290a789d33dfff8525ed44cefeda
Patryk Krakos <redpat20@gmail.com>
2020-11-04 17:20:13 +0100

    Wait for IP

--- setup-node-parity.sh
+++ setup-node-parity.sh
@@ -28,6 +28,15 @@ cp ./node.pwds /l16/
 /snap/parity/current/usr/bin/parity --base-path /l16/chain_data --keys-path /l16/chain_data/keys/AuthorityRound account new --password node.pwds > $NODE_NAME.txt
 mv /l16/chain_data/keys/AuthorityRound/ethereum/* /l16/chain_data/keys/AuthorityRound/
 
+#Wait until all nodes will deliver their IP addresses
+while [ `gsutil du gs://l16-common/ip/*.txt | wc -l` -ne $AUTHORITHIES ]; do
+	#do nothing
+	sleep 5
+done
+
+gsutil cp -r gs://l16-common/ip .
+
+
 #Put address into config
 python3 setConfig.py $NODE_NAME.txt
 cp config.toml /l16/
@@ -39,7 +48,7 @@ gsutil cp ./$NODE_NAME.txt gs://l16-common/addresses
 touch create.lock
 gsutil cp ./create.lock gs://l16-common/addresses
 
-#Wait until all nodes will deliver their addresses
+#Wait until all nodes will deliver their wallet addresses
 while [ `gsutil du gs://l16-common/addresses/*.txt | wc -l` -ne $AUTHORITHIES ]; do
 	#do nothing
 	sleep 5
@@ -66,6 +75,9 @@ if [ `gsutil du gs://l16-common/genesis-parity.json | wc -l` -eq 1 ]
 
 
 fi
+
+#Share IP address
+curl ipinfo.io/ip > ip_$NODE_NAME.txt
 
 #daemonize process
 sudo cp ./parity.service /etc/systemd/system
