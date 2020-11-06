c0d66b71645d5c0cbc6bf240d536c6fefb073697
Patryk Krakos <redpat20@gmail.com>
2020-11-06 15:27:37 +0100

    Fixed script

--- setConfig.py
+++ setConfig.py
@@ -8,9 +8,10 @@ with open ('./' + address_file) as f:
 
 with open('configEmpty.toml') as f:
     data = toml.load(f)
+    data['mining'] = {}
     data['mining']['engine_signer'] = address
     data['mining']['reseal_on_txs'] = 'none'
-    data['mining']['force_sealing'] = true
+    data['mining']['force_sealing'] = True
 
 
 with open('config.toml', 'w') as f:
